b2669be124866c3e23aa086142c55508aa8bc236
Charan reddy <135025143+Charanreddy0007@users.noreply.github.com>
2026-09-13 22:08:50 +0530

    Added Java Solution for 0643_maximum-average-subarray-i

---
+++ Easy/0643_maximum-average-subarray-i/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+
+        int sum = 0;
+
+        for (int i = 0; i < k; i++){
+            sum += nums[i];
+        }
+
+        int max  = sum;
+
+        for (int i = k; i < nums.length; i++){
+            sum += nums[i];
+            sum -= nums[i-k];
+
+            if (max < sum){
+                max = sum;
+            }
+        }
+
+        return (double) max / k;
+    }
+}
