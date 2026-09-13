7c160c4592633c455ed2654fbfd55527b82aa27e
Charan reddy <135025143+Charanreddy0007@users.noreply.github.com>
2026-09-13 22:08:59 +0530

    Added javascript Solution for 0643_maximum-average-subarray-i

---
+++ Easy/0643_maximum-average-subarray-i/Solution.js
@@ -0,0 +1,15 @@
+var findMaxAverage = function(nums, k) {
+    let sum = 0;
+    for (let i = 0; i < k; i++) {
+        sum += nums[i];
+    }
+    let max = sum;
+    for (let i = k; i < nums.length; i++) {
+        sum += nums[i];
+        sum -= nums[i - k];
+        if (max < sum) {
+            max = sum;
+        }
+    }
+    return max / k;
+};
