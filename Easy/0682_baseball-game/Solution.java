1d20aa8525755e3fbd0994562f3a114ec85b97fc
Charan reddy <135025143+Charanreddy0007@users.noreply.github.com>
2026-09-19 21:16:13 +0530

    Added Java Solution for 0682_baseball-game

---
+++ Easy/0682_baseball-game/Solution.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String op : operations) {
+
+            if (op.equals("C")) {
+                stack.pop();
+
+            } else if (op.equals("D")) {
+                stack.push(2 * stack.peek());
+
+            } else if (op.equals("+")) {
+                int last = stack.get(stack.size() - 1);
+                int secondLast = stack.get(stack.size() - 2);
+
+                stack.push(last + secondLast);
+
+            } else {
+                stack.push(Integer.parseInt(op));
+            }
+        }
+
+        int sum = 0;
+
+        for (int score : stack) {
+            sum += score;
+        }
+
+        return sum;
+    }
+}
